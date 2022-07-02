d2e679024ec0aaac3f2a4dfd6ca6a5441145f8cf
NathanBarquira <nathanbarquira@gmail.com>
2022-07-02 13:12:10 -0700

    Started test cases and helper functions

---
+++ helperFunctions.hpp
@@ -0,0 +1,22 @@
+#ifndef DIJKSTRAHELPER_HPP
+#define DIJKSTRAHELPER_HPP
+
+#include <iostream>
+#include <string>
+
+// this is all the declarations:
+
+// basic print function
+template <typename T>
+void print(T text = "");
+
+
+// this is all the definitions:
+
+template <typename T>
+void print(T text)
+{
+    std::cout << text << std::endl;
+}
+
+#endif
---
+++ testCases.cpp
@@ -0,0 +1,11 @@
+#include "helperFunctions.hpp"
+
+int main()
+{
+    print<std::string>();
+    print<std::string>("This is the test cases for the program: ");
+
+    print<std::string>();
+
+    return 0;
+}
