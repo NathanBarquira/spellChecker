186825e97747212eaef57dff8e942172f58899e4
NathanBarquira <nathanbarquira@gmail.com>
2022-07-03 01:34:07 -0700

    updated hash func definition

--- hashFunctions.hpp
+++ hashFunctions.hpp
@@ -4,6 +4,7 @@
 // This will hold all the hash functions that could be used.
 // Can be studied to see which one performs the best.
 
+// This is the Microsoft Research Paul Larson's hash function
 unsigned hashPaulLarson(const char* s, unsigned salt);
 
 #endif
